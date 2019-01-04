b771e000e02001720a9df9bff4a1ccaa3f6d4f18
Vincent Zhang <vincent.zhang@logicmonitor.com>
2019-01-04 14:14:59 +0800

    format lines

--- linesfromfile.scala
+++ linesfromfile.scala
@@ -1,5 +1,23 @@
 import scala.io.Source
 
 // scala linesfromfile.scala linesfromfile.scala
-if (args.length > 0)  Source.fromFile(args(0)).getLines().foreach(println)
-else Console.err.println("linesfromfile <file path>")
+//if (args.length > 0)
+//  Source.fromFile(args(0)).getLines().foreach(println)
+//else
+//  Console.err.println("linesfromfile <file path>")
+
+if (args.length > 0) {
+  val lines =  Source.fromFile(args(0)).getLines().toList
+  val longestLine = lines.reduceLeft((a, b) => longerStr(a, b))
+  val maxWidth = widthOfLength(longestLine)
+  for (elem <- lines) {
+    val paddingCount = maxWidth - widthOfLength(elem)
+    val padding = " " * paddingCount + elem.length
+    println(padding + "|" + elem)
+  }
+}
+else Console.err.println("linesfromfile <filepath>")
+
+def longerStr(a: String, b: String) = if (a.length > b.length) a else b
+
+def widthOfLength(s: String) = s.length.toString.length
